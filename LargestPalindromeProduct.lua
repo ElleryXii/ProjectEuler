deca064e48c622715e9dafc239a4125a74739260
Mason System <ellery.xii@gmail.com>
2020-09-01 22:21:44 +0800

    Improved Problem 4

--- LargestPalindromeProduct.lua
+++ LargestPalindromeProduct.lua
@@ -2,7 +2,6 @@
 -- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. 
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
-
 function CheckPanlindrome(num)
     local numString = tostring(num)
     i = 1
@@ -17,59 +16,15 @@ function CheckPanlindrome(num)
     return true
 end
 
-
--- function CheckDigits(num)
---     local pf = GetPrimeFacotrs(num)
---     mul1, mul2 = 1, 1
---     for _,v in pairs(a) do
---         if mul1*v <= mul2*v then
---             mul1 = mul1 * v
---         else
---             mul2 = mul2 * v
---         end
---         if mul1 > 999 or mul2 > 999 then
---             return false
---         end
---     end
-
---     if mul1<100 or mul2<100 then
---         return false
---     end
---     return true
--- end
-
-
--- function GetPrimeFacotrs(n)
---     local pf = {}
---     idx = 1
---     while (n%2 == 0) do
---         n = n/2
---         pf[idx] = 2
---         idx = idx +1
---     end
-
---     for i=3, math.sqrt(n), 2 do
---         while n%i == 0 do
---             n = n/i
---             pf[idx] = i
---             idx = idx +1 
---         end
---     end
-
---     if n>2 then
---         pf[idx] = n
---     end
-
---     return pf
--- end
-
-
 function Find()
     local max = 0
 
     for i = 999, 100,-1 do
-        for j = 999, 100,-1 do 
+        for j = 999, i,-1 do 
             prod = i*j
+            if prod < max then
+                break
+            end
             if CheckPanlindrome(prod) then
                 max = math.max(max, prod)
             end
--- LargestPrimeFactor.lua
+++ LargestPrimeFactor.lua
@@ -1,23 +1,41 @@
 -- Problem 3 Largest prime factor
 -- What is the largest prime factor of the number 600851475143 ?
+local GetPrimeFactors =  {}
 
 function GetPrimeFacotrs(n)
+    local pf = {}
+    local idx = 1
     while (n%2 == 0) do
-        print(2," ")
+        pf[idx] = 2
+        idx = idx+1
         n = n/2
     end
 
     for i=3, math.sqrt(n), 2 do
         while n%i == 0 do
-            print(i, " ")
+            pf[idx] = i
+            idx = idx+1
             n = n/i
         end
     end
 
     if n>2 then
-        print(n)
+        pf[idx] = n
+        idx = idx+1
+    end
+    return pf
+end
+
+function PrintPrimeFactor(n)
+    local pf = GetPrimeFacotrs(n)
+    for _,v in pairs(pf) do
+        print(v)
     end
 end
 
 
+
+
 GetPrimeFacotrs(600851475143)
+
+return GetPrimeFactors
